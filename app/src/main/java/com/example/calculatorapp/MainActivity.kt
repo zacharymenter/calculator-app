02c1cd14099b34e8bf8d16a2498fd0228ec356c8
menterzj3144 <47538306+menterzj3144@users.noreply.github.com>
2021-09-28 09:54:53 -0500

    Start calculator app

---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.calculatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun numberAction(view: android.view.View) {}
+    fun operationAction(view: android.view.View) {}
+    fun allClearAction(view: android.view.View) {}
+    fun backSpaceAction(view: android.view.View) {}
+    fun equalsAction(view: android.view.View) {}
+}
