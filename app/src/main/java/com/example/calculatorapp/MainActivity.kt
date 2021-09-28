0bb7a1715ff396e304cf0c817b1a1d7caeda79d4
menterzj3144 <47538306+menterzj3144@users.noreply.github.com>
2021-09-28 11:18:07 -0500

    Start adding functionality on button presses

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,16 +2,53 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var canAddOperation = false
+    private var canAddDecimal = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun numberAction(view: android.view.View) {}
-    fun operationAction(view: android.view.View) {}
-    fun allClearAction(view: android.view.View) {}
-    fun backSpaceAction(view: android.view.View) {}
+    fun numberAction(view: android.view.View) {
+        if (view is Button) {
+            if (view.text == ".") {
+                if (canAddDecimal) {
+                    workingsTV.append(view.text)
+                }
+                canAddDecimal = false
+            } else {
+                workingsTV.append(view.text)
+            }
+
+            canAddOperation = true
+        }
+    }
+
+    fun operationAction(view: android.view.View) {
+        if (view is Button && canAddOperation) {
+            workingsTV.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
+        }
+    }
+
+    fun allClearAction(view: android.view.View) {
+        workingsTV.text = ""
+        resultsTV.text = ""
+    }
+
+    fun backSpaceAction(view: android.view.View) {
+        val length = workingsTV.length()
+        if (length > 0)
+            workingsTV.text = workingsTV.text.subSequence(0, length - 1)
+
+    }
+
     fun equalsAction(view: android.view.View) {}
 }
